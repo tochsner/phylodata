ed202139b1d3dfd469975cd88d6b8b2cc8ed9d7b
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-06-30 16:46:20 +0200

    Add WIP disclaimer.

---
+++ website/src/lib/components/footer.svelte
@@ -0,0 +1,8 @@
+<div class="bg-background border-t-accent-light mt-16 flex justify-center rounded-t-3xl border-t-2">
+	<a class="text-accent p-4 underline hover:opacity-70" href="https://github.com/tochsner/evodata">
+		GitHub
+	</a>
+	<a class="text-accent p-4 underline hover:opacity-70" href="mailto:tobia.ochsner@hotmail.com">
+		Feedback
+	</a>
+</div>
--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+	import Footer from '$lib/components/footer.svelte';
+	import { onMount } from 'svelte';
 	import '../app.css';
-	import { Toaster } from 'svelte-5-french-toast';
+	import toast, { Toaster } from 'svelte-5-french-toast';
 
 	let { children } = $props();
+
+	onMount(() => {
+		toast('This website is in development. Not every feature will work.', {
+			duration: 10000
+		});
+	});
 </script>
 
 <Toaster />
 
 <div class="bg-background font-display text-dark min-h-lvh w-lvw">
 	{@render children()}
+	<Footer />
 </div>
