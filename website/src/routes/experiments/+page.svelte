393bf1e0408b026e466f63683ef58363188181ca
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-07-18 14:44:23 +0200

    Add download button on overview page.

--- website/src/routes/experiments/+page.svelte
+++ website/src/routes/experiments/+page.svelte
@@ -8,6 +8,7 @@
 	import type { PageProps } from './$types';
 	import ComboBox from '$lib/components/comboBox.svelte';
 	import { titleCase } from '$lib/utils/titleCase';
+	import DownloadInstructions from '$lib/components/downloadInstructions.svelte';
 
 	let { data }: PageProps = $props();
 	let { papers, possibleSamples, possibleEvolutionaryModels } = data;
@@ -19,6 +20,8 @@
 			filteredPapers = papers;
 		});
 	});
+
+	let experimentsToDownload: [string, number][] | undefined = $state();
 </script>
 
 <Header>
@@ -45,6 +48,31 @@
 				return await update({ reset: false });
 			}}
 	>
+		<button
+			class="border-accent bg-accent flex cursor-pointer items-center space-x-2 rounded-md border px-4 py-2 font-semibold text-white hover:opacity-70"
+			onclick={() =>
+				(experimentsToDownload = filteredPapers
+					.flatMap((paper) => paper.experiments)
+					.map((exp) => [exp.experiment.humanReadableId, exp.experiment.version]))}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M7.5 7.5h-.75A2.25 2.25 0 0 0 4.5 9.75v7.5a2.25 2.25 0 0 0 2.25 2.25h7.5a2.25 2.25 0 0 0 2.25-2.25v-7.5a2.25 2.25 0 0 0-2.25-2.25h-.75m-6 3.75 3 3m0 0 3-3m-3 3V1.5m6 9h.75a2.25 2.25 0 0 1 2.25 2.25v7.5a2.25 2.25 0 0 1-2.25 2.25h-7.5a2.25 2.25 0 0 1-2.25-2.25v-.75"
+				/>
+			</svg>
+
+			<span class="text-base">Use experiments</span>
+		</button>
+
 		{@render filesFilter()}
 		{@render samplesFilter()}
 		{@render treesFilter()}
@@ -178,3 +206,5 @@
 		</div>
 	</a>
 {/snippet}
+
+<DownloadInstructions {experimentsToDownload} />
