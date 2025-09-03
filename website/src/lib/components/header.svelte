944c3159f50a4dbed962aa56226e409451bddb8e
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-09-03 08:25:19 +0200

    Make header nav more responsive.

--- website/src/lib/components/header.svelte
+++ website/src/lib/components/header.svelte
@@ -2,18 +2,18 @@
 	let { children } = $props();
 </script>
 
-<div class="bg-accent-light flex flex-col rounded-b-3xl">
+<div class="bg-accent-light flex flex-col items-stretch rounded-b-3xl">
 	<div class="flex w-full items-center justify-between px-8 py-6">
 		<!-- Logo -->
-		<div class="flex items-center space-x-8">
+		<div class="flex flex-1 flex-col gap-2 space-x-8 sm:flex-row sm:items-center">
 			<h1 class="text-[2rem] font-bold text-gray-800">
 				<span class="text-accent">Phylo</span><span>Data</span>
 			</h1>
 
-			<span class="text-accent/20 text-4xl font-extralight">/</span>
+			<span class="text-accent/20 hidden text-4xl font-extralight sm:block">/</span>
 
 			<!-- Navigation -->
-			<nav class="flex space-x-8">
+			<nav class="flex flex-col space-x-8 sm:flex-row">
 				<a href="/experiments" class="cursor-pointer font-semibold hover:opacity-70">Experiments</a>
 				<a href="/models" class="cursor-pointer font-semibold hover:opacity-70">Models</a>
 				<a href="/docs/python_first_steps" class="cursor-pointer font-semibold hover:opacity-70"
@@ -23,7 +23,7 @@
 		</div>
 
 		<!-- Right side buttons -->
-		<div class="flex items-center space-x-8">
+		<div class="hidden items-center space-x-8 lg:flex">
 			<a
 				href="/new-experiment"
 				class="bg-accent flex cursor-pointer items-center space-x-2 rounded-full px-5 py-3 font-semibold text-white duration-100 hover:scale-[102%] hover:opacity-80"
